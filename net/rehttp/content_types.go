6f3edb4936186d81e52f56dc99a02f1a992993d0
Boris Bobejko <bb@ibclabs.com>
2018-07-30 21:42:24 +0300

    add rehttp - http client sepc

---
+++ net/rehttp/content_types.go
@@ -0,0 +1,3 @@
+package rehttp
+
+type ContentType string
---
+++ net/rehttp/rehttp.go
@@ -0,0 +1,31 @@
+package rehttp
+
+import "github.com/remicro/api/serialization"
+
+type Response interface {
+	Status() (code int)
+	Decoded() (decoded interface{})
+	ContentType() (contentType ContentType)
+	Header(key string) (values []string)
+	Error() (err error)
+}
+
+type Builder interface {
+	GET(url string) Builder
+	POST(url string) Builder
+	DELETE(url string) Builder
+	PATCH(url string) Builder
+	OPTIONS(url string) Builder
+	QueryParam(key, value string) Builder
+	Encoded(object interface{}) Builder
+	ContentType(contentType ContentType) Builder
+	Header(key, value string)
+	Encoder(encoder serialization.Encoder) Builder
+	Decoder(decoder serialization.Decoder) Builder
+	Go() (response Response, err error)
+}
+
+type Factory interface {
+	To(address string) Builder
+}
+
