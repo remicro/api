29fddd3f3ea5e95306b05593edb2f391284ec6de
Boris Bobejko <bb@ibclabs.com>
2018-07-31 21:36:50 +0300

    modify container api

--- ioc/ioc.go
+++ ioc/ioc.go
@@ -1,6 +1,7 @@
 package ioc
 
 type Container interface {
-	Provide(fn interface{})
-	Invoke(fn interface{})
+	Provide(fn interface{}) Container
+	Invoke(fn interface{}) Container
+	Inject() (err error)
 }
