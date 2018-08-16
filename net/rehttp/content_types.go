9f416f6a20b6e15ebfd6de9cab07c285ef40a4dc
Boris Bobejko <bb@ibclabs.com>
2018-08-16 19:19:46 +0300

    add some content types

--- net/rehttp/content_types.go
+++ net/rehttp/content_types.go
@@ -1,3 +1,13 @@
 package rehttp
 
 type ContentType string
+
+func (ct ContentType) String() string {
+	return string(ct)
+}
+
+const (
+	ContentTypeJson = "application/json"
+	ConentTypeXML = "application/xml"
+	ContentTypeHTML = "text/html"
+)
